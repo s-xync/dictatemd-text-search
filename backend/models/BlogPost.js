ccda5104507c8cbe43b93335d7f0a576db913dc9
SaiKumar Immadi <immadi.saikumar@ymail.com>
2023-07-18 07:53:18 +0530

    added blogpost mongoose model and added mongoose boilerplate

---
+++ backend/models/BlogPost.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const blogPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const BlogPost = mongoose.model("BlogPost", blogPostSchema);
+
+module.exports = BlogPost;
